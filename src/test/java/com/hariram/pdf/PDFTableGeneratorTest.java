81842b972287e9c28f1ee4984b61fc17a105b23f
hariram <harirams@am.amrita.edu>
2015-01-08 08:25:25 +0530

    Updating JUnit

--- src/test/java/com/hariram/pdf/PDFTableGeneratorTest.java
+++ src/test/java/com/hariram/pdf/PDFTableGeneratorTest.java
@@ -1,8 +1,11 @@
 package com.hariram.pdf;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -15,7 +18,7 @@ import org.junit.Test;
  */
 public class PDFTableGeneratorTest {
 
-	@Test
+	//@Test
 	public void testTable() {
 		try {
 			Map<Integer, String> columnMap = new TreeMap<Integer, String>();
@@ -48,7 +51,7 @@ public class PDFTableGeneratorTest {
 		}
 	}
 
-	@Test
+	//@Test
 	public void testContent() {
 		try {
 			String[] content = { 
@@ -61,6 +64,47 @@ public class PDFTableGeneratorTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testConvertData() {
+		List<String> list = new ArrayList<String>();
+		Map<String, List<String>> dataMap = new HashMap<String, List<String>>();
+		
+		list.add("1.0");
+		list.add("1.5");
+		list.add("2.0");
+		list.add("2.5");
+		dataMap.put(null, list);
+		
+		list = new ArrayList<String>();
+		list.add("2.0");
+		list.add("2.5");
+		list.add("3 .0");
+		list.add("3.5");
+		//list.add("3.0");
+		dataMap.put("B", list);
+
+		Map<Integer, String> columnMap = new TreeMap<Integer, String>();
+		columnMap.put(1, "col1-100");
+		columnMap.put(2, "col2-100");
+		columnMap.put(3, "col3-100");
+		columnMap.put(4, "col4-100");
+		
+		
+		try {
+			String[][] content = PDFUtil.convertData(dataMap);
+			System.out.println(content);
+			
+			Table table = PDFUtil.createContent(columnMap, content);
+			new PDFGenerator().generatePDF(table,"table.pdf");
+		} catch (COSVisitorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
